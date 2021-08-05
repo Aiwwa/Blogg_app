12ce86183d511bf545aad34cc371a18abe593e00
Aiwwa <as.savinis@gmail.com>
2021-08-05 20:06:52 +0300

    first commit

---
+++ src/components/SinglePost.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="post">
+    <router-link :to="{ name: 'Details', params: { id: post.id } }">
+      <h3>{{ post.title }}</h3>
+    </router-link>
+    <p>{{ snippet }}</p>
+    <span v-for="tag in post.tags" :key="tag">#{{ tag }}</span>
+  </div>
+</template>
+
+<script>
+import { computed } from 'vue'
+
+export default {
+  props: ['post'],
+  setup(props) {
+    const snippet = computed(() => {
+      return props.post.body.substring(0, 100) + '...'
+    })
+
+    return { snippet }
+  },
+}
+</script>
+
+<style>
+.post {
+  margin: 0 40px 30px;
+  padding-bottom: 30px;
+  border-bottom: 1px dashed #e7e7e7;
+  background: #eeeeee;
+}
+.post h3 {
+  display: inline-block;
+  position: relative;
+  font-size: 20px;
+  margin-bottom: 10px;
+  max-width: 400px;
+  color: black;
+}
+</style>
